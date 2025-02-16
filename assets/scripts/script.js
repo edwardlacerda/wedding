24b0f8c1cf97f2c58c018e17c852c78dac87e678
Edward Lacerda <elacerda28@gmail.com>
2025-02-16 00:32:59 -0300

    add conexao com railway

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,10 +8,11 @@ app.use(express.json());
 
 // Configuração da conexão com o banco de dados
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "wedding",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
